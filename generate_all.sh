ae5829d43fc5556be1969b4185bcdcd6a5b4c568
Gregory Dzhanelidze <350804+strlght@users.noreply.github.com>
2023-11-01 21:58:32 +0100

    feat: add `Last modified` and update expires

--- generate_all.sh
+++ generate_all.sh
@@ -5,6 +5,6 @@ FILTER_FILE=filters/all.txt
 [ -f "$FILTER_FILE" ] && rm "$FILTER_FILE"
 
 (
-  cat helpers/header.txt
+  sed -r "s/^! Last modified:/! Last modified: $(date -Ru)/g" helpers/header.txt
   find filters/ -type f -name '*.txt' -not -name 'all.txt' -print0 | xargs -0 grep -h "^[^!;]" --
 ) > "$FILTER_FILE"
