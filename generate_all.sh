6529dbf48deea94409e425361abff8336f8d6e05
Gregory Dzhanelidze <350804+strlght@users.noreply.github.com>
2023-11-01 21:42:49 +0100

    feat: deploy to pages

---
+++ generate_all.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+FILTER_FILE=filters/all.txt
+
+[ -f "$FILTER_FILE" ] && rm "$FILTER_FILE"
+
+(
+  cat helpers/header.txt
+  find filters/ -type f -name '*.txt' -not -name 'all.txt' -print0 | xargs -0 grep -h "^[^!;]" --
+) > "$FILTER_FILE"
